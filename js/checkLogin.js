c9488483da357fb25cfded93aeae8f3cacfd6928
kiingdavies <daviesadedeji007@gmail.com>
2019-09-16 19:39:11 +0100

    Loan app checkLogin.js

---
+++ js/checkLogin.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  //Check if there is any user data stored in the local storage
+  //because user data is stored in localstorage at login
+  let user = window.localStorage.getItem('email');
+  if (!user) {
+    //If no user data, redirect to signup/login page, anyone you like
+    $('.checkLogin').html('Kindly Log in');
+    window.location.assign('signup.html');
+  } else {
+    //Else prompt the user he is logged in
+    $('.checkLogin').html('You are logged in');
+  }
+});
