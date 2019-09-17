46ec942b4aac8434c48bc7276fa7d5894e088bdf
kiingdavies <daviesadedeji007@gmail.com>
2019-09-17 10:26:58 +0100

    forms js update 09/17

--- js/forms.js
+++ js/forms.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
   //Over sabi Effect
   $('.registerBtn').click(function() {
     $('.regForm').fadeIn();
+    $('.loginForm').hide();
   });
   $('.closeBtn').click(function() {
     $('.regForm').fadeOut();
   });
   $('.loginBtn').click(function() {
+    $('.regForm').hide();
     $('.loginForm').fadeIn();
   });
   $('.closeLoginBtn').click(function() {
+  
     $('.loginForm').fadeOut();
   });
+
   //Registration Function
   $('.regSubmitBtn').click(function(event) {
     event.preventDefault();
@@ -19,21 +23,25 @@ $(document).ready(function() {
     const username = $('#username').val();
     const password = $('#password').val();
     const email = $('#email').val();
+
     //Check if user input is empty
     if (!fullname || !username || !password || !email) {
       $('.regMessage').html('Kindly fill in all fields');
       return;
     }
+
     //Make get request to check if the user already exist
     $.ajax({
       method: 'GET',
-      url: `http://localhost:3000/users?email=${email}`,
+      url: `http://localhost:3000/user?email=${email}`,
       data: {
         email,
       },
+
       beforeSend: function() {
         $('.regMessage').html('Loading....');
       },
+
       success: function(response) {
         if (response.length) {
           $('.regMessage').html('User already exist');
@@ -41,7 +49,7 @@ $(document).ready(function() {
           //Submit the user data if the user does not exist
           $.ajax({
             method: 'POST',
-            url: 'http://localhost:3000/users',
+            url: 'http://localhost:3000/user',
             data: {
               fullname,
               username,
@@ -59,6 +67,7 @@ $(document).ready(function() {
       },
     });
   });
+
   //Login Function
   $('.loginSubmitBtn').click(function(event) {
     event.preventDefault();
@@ -68,10 +77,11 @@ $(document).ready(function() {
       $('.regMessage').html('Kindly fill in all fields');
       return;
     }
+
     //Check if the user is in the database
     $.ajax({
       method: 'GET',
-      url: `http://localhost:3000/users?email=${emailLogin}&password=${passwordLogin}`,
+      url: `http://localhost:3000/user?email=${emailLogin}&password=${passwordLogin}`,
       data: {
         email: emailLogin,
         password: passwordLogin,
@@ -84,14 +94,16 @@ $(document).ready(function() {
           $('.regMessage').html('Login sucessful');
           $('.checkLogin').html('You are logged in');
           localStorage.setItem('email', emailLogin);
+
           //redirect to home page if the login is successfull
-          window.location.assign('index.html');
+          window.location.assign('index.html'); //send to the dashborad not index again
         } else {
           $('.regMessage').html('Username or password Incorrect');
         }
       },
     });
   });
+
   //Logout Function
   $('.logoutBtn').click(function() {
     //clear the localstorage and redirect to signup page
